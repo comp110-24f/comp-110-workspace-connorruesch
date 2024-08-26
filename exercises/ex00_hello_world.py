2e2cf457b01de199335b530663c06c6b63d82cb5
connorruesch <connorruesch@outlook.com>
2024-08-26 11:43:02 -0400

    hello world exercise & functions lesson

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP110"""
+
+__author__ = "730736398"
+
+
+def greet(name: str) -> str:
+    """A welcomeing first function definition."""
+    return "Hello, " + name + "!"
