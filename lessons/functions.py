24296af20d5865a57fb48332a2880aa64412d21b
connorruesch <connorruesch@outlook.com>
2024-08-26 12:18:34 -0400

    update functions lessons to include sum function definition and syntax notes

--- lessons/functions.py
+++ lessons/functions.py
@@ -1,5 +1,18 @@
 """Practice with functions."""
 
-from random import randint
+# from random import randint
 
-print(randint(3, 17))
+# print(randint(10, 50))
+
+
+# Function Definition
+def sum(num1: int, num2: int) -> int:
+    """Calculate the sum of num1 and num2, then return it."""
+    return num1 + num2
+
+
+# Syntax note -- parameters are going to be within the function
+# signature, arguments are within the function call.
+
+# Function Call
+print(sum(num1=2, num2=3))  # <- arguments
