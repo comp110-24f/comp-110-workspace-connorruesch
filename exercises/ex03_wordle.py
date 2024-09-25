1a7a18076f4364370377c3688d1298ec34714310
connorruesch <connorruesch@outlook.com>
2024-09-25 13:11:32 -0400

    begin wordle exercise

---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,23 @@
+"""A recreation of the popular New York Times game, Wordle."""
+
+__author__: str = "730739368"
+
+
+def input_guess(secret_word_length: int) -> str:
+    """Checks to make sure the guess word is the same length as the
+    secret word--if it is, we return the guess word."""
+
+    guess_word: str = input(f"Enter a {secret_word_length} character word: ")
+
+    # Had to be a little more creative with this, since we have two potential cases
+    # that we're trying to prevent: if the number of the characters in the guess word
+    # is less than the length of the secret word, and if it is higher. My solution: if
+    # it isn't, we just continually prompt the user for an input, and keep changing the
+    # guess word variable until they enter a word of the correct length.
+    while len(guess_word) != secret_word_length:
+        # Since python's input() function halts the program execution until a user
+        # enters an input, all we have to do is change our guess_word variable
+        # and continually evaluate what the length of it is.
+        guess_word = input(f"That wasn't {secret_word_length} chars! Try again: ")
+
+    return guess_word
