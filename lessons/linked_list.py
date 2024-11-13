0ca68d9d8d347d1369b5fad40700ffd8309c2d50
connorruesch <connorruesch@outlook.com>
2024-11-13 11:20:34 -0500

    linked list base

---
+++ lessons/linked_list.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+
+class Node:
+    value: int
+    next: Node | None
+
+    def __init__(self, value: int, next: Node | None):
+        self.value = value
+        self.next = next
+
+
+two: Node = Node(2, None)
+one: Node = Node(1, two)
+courses: Node = Node(110, Node(210, Node(301, None)))
+
+
+def to_str(head: Node | None) -> str:
+    """Represent a linked list as a str."""
+    if head is None:
+        return "None"
+    else:
+        rest: str = to_str(head.next)
+        return f"{head.value} -> {rest}"
+
+
+print(to_str(one))
+print(to_str(courses))
